51c07ee045dcafe7afc9609493d0c357b33ab334
zulfiya-amrul <zulfiya.amrul@gmail.com>
2022-05-21 10:09:19 -0600

    this commit has the basic framework structure, steps, pages, utils

---
+++ src/test/java/steps/PageInitializers.java
@@ -0,0 +1,29 @@
+package steps;
+
+
+import pages.DashboardPage;
+import pages.LoginPage;
+
+public class PageInitializers {
+    // this is were we create objects of all pages and we initialize them
+    // this page is in inheritance with common method --- common method extends PageInitializers
+
+    public static LoginPage login;
+   /* public static EmployeeSearchPage employeeSearchPage;
+    public static AddEmployeePage addEmployeePage;
+
+    */
+    public static DashboardPage dash;
+
+
+    public static void intializePageObjects() {
+        login = new LoginPage();
+        /*
+        employeeSearchPage = new EmployeeSearchPage();
+        addEmployeePage = new AddEmployeePage();
+
+         */
+        dash = new DashboardPage();
+
+    }
+}
